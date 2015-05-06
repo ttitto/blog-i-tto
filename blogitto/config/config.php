bcd2fa694ee1fca1a7643045a280123a34f32888
ttitto <ttitto_prog@vfemail.net>
2015-05-07 00:56:10 +0300

    url parsing implemented in Router constructor

--- blogitto/config/config.php
+++ blogitto/config/config.php
@@ -1,2 +1,14 @@
 <?php
 \Blogitto\Config::set('site_name', 'Blog-i-tto');
+\Blogitto\Config::set('languages', array('en', 'bg'));
+
+\Blogitto\Config::set('routes', array(
+    'default' => '',
+    'admin' => 'admin_',
+    'user' => 'user_'
+));
+
+\Blogitto\Config::set('default_route', 'default');
+\Blogitto\Config::set('default_language', 'en');
+\Blogitto\Config::set('default_controller', 'Posts');
+\Blogitto\Config::set('default_action', 'index');
--- blogitto/lib/Router.php
+++ blogitto/lib/Router.php
@@ -2,17 +2,55 @@
 
 namespace Blogitto;
 
-class Router {
+class Router
+{
     protected $uri;
     protected $controller;
     protected $action;
     protected $params;
     protected $route;
     protected $method_prefix;
+    protected $language;
 
-    public  function  __construct(){
-        // TODO:
-        echo ' test';
+    public function  __construct($uri)
+    {
+        $this->uri = urldecode(trim($uri, '/'));
+
+        $routes = \Blogitto\Config::get('routes');
+
+        $this->route = \Blogitto\Config::get('default_route');
+        $this->method_prefix = isset($routes[$this->route]) ? $routes[$this->route] : \Blogitto\Config::get('routes')['default'];
+        $this->controller = \Blogitto\Config::get('default_controller');
+        $this->action = \Blogitto\Config::get('default_action');
+        $this->language = \Blogitto\Config::get('default_language');
+
+        // Parse url like /lng/method_prefix/controller/action/param1/param2/...
+        $uri_parts = explode('?', $this->uri);
+        $path = $uri_parts[0];
+        $path_parts = explode('/', $path);
+
+        if (count($path_parts)) {
+            if (in_array(strtolower(current($path_parts)), array_keys($routes))) {
+                $this->route = strtolower(current($path_parts));
+                $this->method_prefix = isset($routes[$this->route]) ? $routes[$this->route] : \Blogitto\Config::get('routes')['default'];
+                array_shift($path_parts);
+            } elseif (in_array(strtolower(current($path_parts)), \Blogitto\Config::get('languages'))) {
+                $this->language = strtolower(current($path_parts));
+                array_shift($path_parts);
+            }
+
+            if (current($path_parts)) {
+                $this->controller = ucfirst(strtolower(current($path_parts)));
+                array_shift($path_parts);
+            }
+
+            if (current($path_parts)) {
+                $this->action = strtolower(current($path_parts));
+                array_shift($path_parts);
+            }
+
+            $this->params = $path_parts;
+        }
     }
 
     /**
@@ -61,6 +99,14 @@ class Router {
     public function getUri()
     {
         return $this->uri;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getLanguage()
+    {
+        return $this->language;
     }
 
 
